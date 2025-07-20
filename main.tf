d45af1fbafad1a3d98ca3c26982bcdcbaa5c1c2f
gaurav Shukla <gauravshukla2548@gmail.com>
2025-07-20 23:23:02 +0530

    pahla pahla payar hai

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+     azurerm={
+         source = "hashicorp/azurerm"
+          version = "4.37.0"
+
+}
+
+  }
+}
+
+provider "azurerm" {
+
+    feature{}
+    subscription_Id="3f7c4872-f198-45b7-984d-0ab188b016e6"
+  
+}
+
+
+resource "azurerm_resource_group" "rg_name" {
+    name= "Shikha"
+    location="Eastus"
+  
+}
