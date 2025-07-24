1310ca324f85f6e69ef7e04021318ceba34bd2e4
gaurav Shukla <gauravshukla2548@gmail.com>
2025-07-24 07:35:28 +0530

    mere dil

--- main.tf
+++ main.tf
@@ -22,3 +22,31 @@ resource "azurerm_resource_group" "rg_name" {
     location="Eastus"
   
 }
+
+resource "azurerm_virtual_network" "vnet_name" {
+  name                = "ShikhaVnet"
+  address_space       = ["10.0.0.16/16"]
+  resource_group_name = "shikha"
+  location            = "eastus"
+  
+}
+
+resource "azurerm_subnet" "subnet_name" {
+  name                 = "ShikhaSubnet"
+  resource_group_name  = "shikha"
+  virtual_network_name = "ShikhaVnet"
+  address_prefixes     = ["10.0.0.0/24"]
+
+}
+
+resource "azurerm_network_interface" "nic_name" {
+  name                = "ShikhaNIC"
+  location            = azurerm_resource_group.rg_name.location
+  resource_group_name = azurerm_resource_group.rg_name.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_name.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
