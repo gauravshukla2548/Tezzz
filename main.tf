8f5e189b470f1caf1c1fc2c5bef3dd3a62b9a3ec
gaurav Shukla <gauravshukla2548@gmail.com>
2025-07-24 08:43:38 +0530

    merra dil

--- main.tf
+++ main.tf
@@ -58,3 +58,28 @@ resource "azurerm_public_ip" "pub-ip_name" {
   allocation_method   = "Dynamic"
   
 }
+
+resource "azurerm_linux_virtual_machine" "vm_name" {
+  name                = "ShikhaVM"
+  resource_group_name = azurerm_resource_group.rg_name.name
+  location            = azurerm_resource_group.rg_name.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd1234!"
+  
+  network_interface_ids = [
+    azurerm_network_interface.nic_name.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
